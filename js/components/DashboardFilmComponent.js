e6a290dbc273d418419a7bd414f65c7af0433ee8
mariamkhalifa <m_khalifa@fanhsaweonline.ca>
2020-03-25 11:34:59 -0400

    colors and logo

--- js/components/DashboardFilmComponent.js
+++ js/components/DashboardFilmComponent.js
@@ -38,19 +38,22 @@ export default {
                 <h1 class="hidden">Bottom Navigation</h1>
                 <li>
                     <router-link to="/dashboardfilm">
-                        <i class="fas fa-film  fa-4x"></i>
+                        <!-- <i class="fas fa-film  fa-4x"></i> -->
+                        <img src="images/film.svg" alt="film icon">
                         <p>Film</p>
                     </router-link>
                 </li>
                 <li>
                     <router-link to="/dashboardtv">
-                        <i class="fas fa-tv fa-4x"></i>
+                        <!-- <i class="fas fa-tv fa-4x"></i> -->
+                        <img src="images/tv.svg" alt="tv icon">
                         <p>Tv</p>
                     </router-link>
                 </li>
                 <li>
                     <router-link to="/dashboardmusic">
-                        <i class="fas fa-music fa-4x"></i>
+                        <!-- <i class="fas fa-music fa-4x"></i> -->
+                        <img src="images/music.svg" alt="music icon">
                         <p>Music</p>
                     </router-link>
                 </li>
